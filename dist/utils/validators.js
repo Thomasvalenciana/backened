7f6bb01496430d11de34f170af798919d03daa47
Thomasvalenciana <123031751+Thomasvalenciana@users.noreply.github.com>
2024-10-26 19:27:20 -0500

    user login

--- dist/utils/validators.js
+++ dist/utils/validators.js
@@ -1,5 +1,5 @@
-import { body } from "express-validator";
-const validate = (validations) => {
+import { body, validationResult } from "express-validator";
+export const validate = (validations) => {
     return async (req, res, next) => {
         for (let validation of validations) {
             const result = await validation.run(req);
@@ -7,14 +7,22 @@ const validate = (validations) => {
                 break;
             }
         }
+        const errors = validationResult(req);
+        if (errors.isEmpty()) {
+            return next();
+        }
+        return res.status(422).json({ errors: errors.array() });
     };
 };
-const signupValidator = [
-    body("name").notEmpty().withMessage("name is required"),
+export const loginValidator = [
     body("email").trim().isEmail().withMessage("email is required"),
     body("password")
         .trim()
         .isLength({ min: 6 })
         .withMessage("Password should contain at least 6 characters."),
+];
+export const signupValidator = [
+    body("name").notEmpty().withMessage("name is required"),
+    ...loginValidator,
 ];
 //# sourceMappingURL=validators.js.map
--- src/controllers/user-controllers.ts
+++ src/controllers/user-controllers.ts
@@ -20,10 +20,21 @@ export const userSignup = async (req: Request,res: Response,next: NextFunction )
         const hashedPassword = await hash(password, 10)
         const user = new User({ name,email,password: hashedPassword });
         await user.save();
-        return res.status(200).json({message:"OK", id: user._id.toString() });        
-      } catch (error){
+        return res.status(201).json({message:"OK", id: user._id.toString() });        
+    } catch (error){
        console.log(error);
-       return res.status(200).json({message: "ERROR", cause: error.message});
-      }
-
+       return res.status(200).json({message: "ERROR", cause: error.message}); 
+    }
+};
+export const userLogin = async (req: Request,res: Response,next: NextFunction ) => {
+    try{
+        // user login
+        const {email,password } =req.body;
+        const user = await User.findOne({email})
+        
+       //  return res.status(201).json({message:"OK", id: user._id.toString() });        
+    } catch (error){
+       console.log(error);
+       return res.status(200).json({message: "ERROR", cause: error.message}); 
+    }
 };
